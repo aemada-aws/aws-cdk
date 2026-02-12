c290f6354693d7b761eb8e02e080a7e7eba20e50
Ahmed Hamed <aemada@amazon.com>
2026-02-12 11:42:00 +0000

    chore(apprunner-alpha): extract region constant, add missing region specs, add IntegTest wrapper to ecr-public, remove duplicate .ts.snapshot

---
+++ packages/@aws-cdk/aws-apprunner-alpha/test/apprunner-supported-regions.ts
@@ -0,0 +1,6 @@
+// https://docs.aws.amazon.com/apprunner/latest/dg/what-is-apprunner.html
+export const APPRUNNER_SUPPORTED_REGIONS = [
+  'ap-northeast-1', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2',
+  'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3',
+  'us-east-1', 'us-east-2', 'us-west-2',
+];
